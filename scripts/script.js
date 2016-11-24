e657c39d0ee879946b29d44859d672349b1901c8
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-24 11:28:51 -0700

    organizing page layout

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,21 @@ var customerTypes = ["Angry", "Drunk", "Dumb", "First Timer", "Know It All", "Al
 
 var coWorkerTypes = ["Flirty", "Drunk", "Angry", "Lazy", "Type A", "Cheery", "Company Man"];
 var coWorkerRoles = ["Line", "Cook", "Bartender", "Supervisor"];
+
+var playerName = '';
+
+$('.playerName').submit(function(event) {
+    event.preventDefault();
+    playerName = $('input[name=newPlayer]').val();
+});
+
+function getNewCustomer(){
+  var newCustomer = {};
+  newCustomer.name =
+  return newCustomer;
+};
+
+function getNewCoworker(){
+  
+
+};
