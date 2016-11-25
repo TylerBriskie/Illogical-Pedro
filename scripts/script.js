e9595afd2ffc556953bb8c06e2824153667c9fc6
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-25 12:14:25 -0700

    working on random customer generator

--- scripts/script.js
+++ scripts/script.js
@@ -10,13 +10,18 @@ $('.playerName').submit(function(event) {
     playerName = $('input[name=newPlayer]').val();
 });
 
-function getNewCustomer(){
-  var newCustomer = {};
-  newCustomer.name =
-  return newCustomer;
-};
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: getNewCustomer(data),
+});
 
-function getNewCoworker(){
-  
-
+var getNewCustomer = function(data){
+  this.firstName = data.results[0].name.first;
+  this.lastName = data.results[0].name.last;
+  this.drunk = Math.floor(Math.random()*10);
+  this.flirty = Math.floor(Math.random()*10);
+  this.angry = Math.floor(Math.random()*10);
+  this.cool = Math.floor(Math.random()*10);
+  this.exp = Math.floor(Math.random()*10);
 };
